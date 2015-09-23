191bfc91039f6bf3af46844d00f59b13149f1238
Joey Tsai <a-jotsai@expedia.com>
2015-09-23 16:23:56 -0700

    Maybe code changes worth more?

--- src/main/scala/com/lxhub/sbt/plugin/Herro.scala
+++ src/main/scala/com/lxhub/sbt/plugin/Herro.scala
@@ -10,5 +10,5 @@ object Herro extends App {
   val sha = BuildInfoLxSbtPlugin.gitHeadCommit
   println(s"oh hai, my version=$ver sha1=$sha")
   // make more changes.
-  // just to be sure the version doesn't get updated by sbt-git
+  println("is tehre more?")
 }
